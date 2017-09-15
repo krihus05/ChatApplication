f5877d62bb25fdc6f597684c1907ecbd4285af24
Kristian F Hustad <krihus05@gmail.com>
2017-09-15 16:23:48 +0200

    Add files via upload

--- src/main/java/com/krifhu/chatapplication/ConversationServices.java
+++ src/main/java/com/krifhu/chatapplication/ConversationServices.java
@@ -34,24 +34,37 @@ public class ConversationServices {
     EntityManager em;
     
     @GET
-    @Path("get")
+    @Path("getAll")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getMessages(){
-        
-        String getMessages = "getMessages!";
-        
-        return Response.ok(getMessages).build();
+    public List<Messages> getMessages(){
+        List<Messages> result = null; 
+        result = em.createQuery("SELECT m FROM Messages m", Messages.class)
+                .getResultList();
+        return result != null ? result : Collections.EMPTY_LIST;
+    }
+    
+    @GET
+    @Path("getConversation")
+    @Produces(MediaType.APPLICATION_JSON) 
+    public List<Messages> getConversation(@QueryParam("user1") String user1, @QueryParam("user2") String user2){ 
+        List<Messages> result = null;   
+        result = em.createQuery("SELECT m FROM Messages m WHERE (m.sender = :user1 AND m.receiver = :user2) OR (m.sender = :user2 AND m.receiver = :user1)", Messages.class)
+                .setParameter("user1", user1)
+                .setParameter("user2", user2)
+                .getResultList();
+        return result != null ? result : Collections.EMPTY_LIST;
     }
     
     @POST
     @Path("add")
-    public Response addMessage(@QueryParam("message") String message){
-        System.out.println(message);
-        
-        //String addMessage = "addMessage";
-        
-        return Response.ok(message).build();
-    }
-    
-    
+    public Response addMessage(@QueryParam("sender") String sender, @QueryParam("receiver") String receiver, @QueryParam("messageBody") String messageBody) {
+        if (sender != null && receiver != null && messageBody != null) {
+            Messages m = new Messages();
+            em.persist(m);
+            m.setSender(sender);
+            m.setReceiver(receiver);
+            m.setMessageBody(messageBody);
+        }
+        return Response.ok(sender).build();
+    }  
 }
